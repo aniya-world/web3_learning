67d1988fc4b7d1a1d258593eca2ec5a0f9ec014c
aniya-world <aniyaa@duck.com>
2025-04-30 21:36:35 +0800

    250430

--- level_2/2.Go-Ethereum开发入门/2.06 代币转账/main.go
+++ level_2/2.Go-Ethereum开发入门/2.06 代币转账/main.go
@@ -45,11 +45,22 @@ func main() {
     if err != nil {
         log.Fatal(err)
     }
-
+    // gasPrice = big.NewInt(0).Add(gasPrice, big.NewInt(10000000000))
+    // 创建一个新的 big.Int 对象，初始值为 0。big.Int 是 Go 语言中用于处理大整数的类型，适用于以太坊中常用的 wei 单位。
+    // Add 方法用于将两个 big.Int 对象相加。在这里，它将当前的 gasPrice（即从以太坊网络获取的建议 gas 价格）与一个新的 big.Int 对象（其值为 10 Gwei，即 10,000,000,000 wei）相加。
+    // 10 Gwei 是一个常见的 gas 价格增量，通常用于确保交易在网络中更快地被处理。
     toAddress := common.HexToAddress("0x4592d8f8d7b001e72cb26a73e4fa1806a51ac79d") // 将目标地址（接收者地址）从十六进制字符串转换为以太坊地址
     tokenAddress := common.HexToAddress("0x28b149020d2152179873ec60bed6bf7cd705775d") // 将代币合约地址从十六进制字符串转换为以太坊地址
 
     transferFnSignature := []byte("transfer(address,uint256)") // 定义 ERC20 代币合约的 transfer 函数的签名，用于构造交易数据
+    /*
+    这行代码的目的是将 transfer 函数的签名转换为字节切片，以便在后续的交易数据构造中使用。
+    具体来说，ERC20 代币的 transfer 函数在以太坊网络中调用时，需要将函数签名的哈希值作为交易数据的一部分。
+
+    ## 使用场景
+    在构造 ERC20 代币转账交易时，您需要将函数签名的哈希值（前 4 个字节）作为交易数据的开头，后面跟着接收者地址和转账金额。
+    这样，矿工在处理交易时就能识别出这是一个调用 transfer 函数的请求。
+    */
     hash := sha3.NewLegacyKeccak256() // 创建一个新的 Keccak256 哈希对象，用于计算函数签名的哈希值
     hash.Write(transferFnSignature) // 函数签名写入哈希对象
     methodID := hash.Sum(nil)[:4]   // 计算哈希值并取前 4 个字节，作为方法 ID
@@ -79,6 +90,8 @@ func main() {
     if err != nil {
         log.Fatal(err)
     }
+
+    // gasLimit := uint64(100000)
     fmt.Println(gasLimit) // 23256
 
     // 创建一个新的交易对象，包含 nonce、代币合约地址、转账金额（0 wei）、估算的 gas 限制、建议的 gas 价格和构造的交易数据
