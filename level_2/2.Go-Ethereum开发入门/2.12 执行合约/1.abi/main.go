756006b449ae0789e8cbd443c2eec0574ddaf399
aniya-world <aniyaa@duck.com>
2025-06-10 00:06:39 +0800

    .

---
+++ level_2/2.Go-Ethereum开发入门/2.12 执行合约/1.abi/main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"errors"
+	"fmt"
+	"log"
+	"math/big"
+	"strings"
+	"time"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+const (
+	contractAddr = "0x1399E0Fa83fBc9d1f34E16fe8603CF1348aA943B"
+)
+
+func main() {
+	client, err := ethclient.Dial("https://eth-sepolia.g.alchemy.com/v2/9LURSvm6osXr98M_7j_AfY4fdhs2J9WL")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privateKey, err := crypto.HexToECDSA("38edd948f94c292109f1ab6a8315a6d1c2934ee0f514047d41cb7bb25087ff4c")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 获取公钥地址
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("error casting public key to ECDSA")
+	}
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+
+	// 获取 nonce
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 估算 gas 价格
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 准备交易数据
+	contractABI, err := abi.JSON(strings.NewReader(`[{"inputs":[{"internalType":"string","name":"_version","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"bytes32","name":"key","type":"bytes32"},{"indexed":false,"internalType":"bytes32","name":"value","type":"bytes32"}],"name":"ItemSet","type":"event"},{"inputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"name":"items","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"key","type":"bytes32"},{"internalType":"bytes32","name":"value","type":"bytes32"}],"name":"setItem","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"version","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"}]`))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	methodName := "setItem"
+	var key [32]byte
+	var value [32]byte
+
+	copy(key[:], []byte("demo_save_key_use_abi_12"))
+	copy(value[:], []byte("demo_save_value_use_abi_1222"))
+	input, err := contractABI.Pack(methodName, key, value)
+
+	// 创建交易并签名交易
+	chainID := big.NewInt(int64(11155111))
+	tx := types.NewTransaction(nonce, common.HexToAddress(contractAddr), big.NewInt(0), 300000, gasPrice, input)
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 发送交易
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Transaction sent: %s\n", signedTx.Hash().Hex())
+	_, err = waitForReceipt(client, signedTx.Hash())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 查询刚刚设置的值
+	callInput, err := contractABI.Pack("items", key)
+	if err != nil {
+		log.Fatal(err)
+	}
+	to := common.HexToAddress(contractAddr)
+	callMsg := ethereum.CallMsg{
+		To:   &to,
+		Data: callInput,
+	}
+
+	// 解析返回值
+	result, err := client.CallContract(context.Background(), callMsg, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var unpacked [32]byte
+	contractABI.UnpackIntoInterface(&unpacked, "items", result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("is value saving in contract equals to origin value:", unpacked == value)
+}
+
+func waitForReceipt(client *ethclient.Client, txHash common.Hash) (*types.Receipt, error) {
+	for {
+		receipt, err := client.TransactionReceipt(context.Background(), txHash)
+		if err == nil {
+			return receipt, nil
+		}
+		if !errors.Is(err, ethereum.NotFound) {
+			return nil, err
+		}
+		// 等待一段时间后再次查询
+		time.Sleep(1 * time.Second)
+	}
+}
+
+/*
+Transaction sent: 0x0df0a37b1cc8b5192d2d0e602eb0a6312b183e5b254c3dcad1e1f6a16ea0dfa6
+is value saving in contract equals to origin value: true
+
+*/
